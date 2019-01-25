c0834e88d2b2be99177d6be95eba1827e87aa1e2
wheat <1317839864@qq.com>
2019-01-25 20:38:34 +0800

    git yao

--- yolo/config.py
+++ yolo/config.py
@@ -6,7 +6,7 @@ import os
 
 DATA_PATH = 'data'
 
-PASCAL_PATH = os.path.join(DATA_PATH, 'pascal_voc')
+PASCAL_PATH = os.path.join(DATA_PATH, 'pas_real')
 
 CACHE_PATH = os.path.join(PASCAL_PATH, 'cache')
 
@@ -14,16 +14,19 @@ OUTPUT_DIR = os.path.join(PASCAL_PATH, 'output')
 
 WEIGHTS_DIR = os.path.join(PASCAL_PATH, 'weights')
 
-WEIGHTS_FILE = None
+# WEIGHTS_FILE = None
 # WEIGHTS_FILE = os.path.join(DATA_PATH, 'weights', 'YOLO_small.ckpt')
+WEIGHTS_FILE = './data/pas_real/output/2019_01_24_17_00/yolo-1000'
 
+'''''
 CLASSES = ['aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus',
            'car', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse',
            'motorbike', 'person', 'pottedplant', 'sheep', 'sofa',
            'train', 'tvmonitor']
-
+'''''
 FLIPPED = True
 
+CLASSES = ['realsense']
 
 #
 # model parameter
@@ -49,9 +52,9 @@ COORD_SCALE = 5.0
 # solver parameter
 #
 
-GPU = ''
+GPU = '0'
 
-LEARNING_RATE = 0.0001
+LEARNING_RATE = 0.01
 
 DECAY_STEPS = 30000
 
@@ -59,9 +62,9 @@ DECAY_RATE = 0.1
 
 STAIRCASE = True
 
-BATCH_SIZE = 45
+BATCH_SIZE = 1
 
-MAX_ITER = 15000
+MAX_ITER = 5000
 
 SUMMARY_ITER = 10
 
